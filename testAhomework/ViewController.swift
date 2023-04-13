956b3ac6a073ac46c39cd375cd3a3edcea529cf6
AlexShkunov <aws8q7@gmail.com>
2023-04-13 17:54:04 +0300

    Add homework 3

--- testAhomework/ViewController.swift
+++ testAhomework/ViewController.swift
@@ -29,6 +29,12 @@ class ViewController: UIViewController {
         return view
     }()
     
+    private let greenView: UIView = {
+        $0.backgroundColor = .green
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIView())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         layout()
@@ -38,25 +44,32 @@ class ViewController: UIViewController {
         view.addSubview(redView)
         view.addSubview(blueView)
         view.addSubview(yellowView)
+        view.addSubview(greenView)
         
         NSLayoutConstraint.activate([
             // redView
             redView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
             redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
             redView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
-            redView.heightAnchor.constraint(equalToConstant: 250),
+            redView.heightAnchor.constraint(equalToConstant: 150),
             
             // blueView
             blueView.topAnchor.constraint(equalTo: redView.bottomAnchor, constant: 20),
             blueView.leadingAnchor.constraint(equalTo: redView.leadingAnchor),
             blueView.trailingAnchor.constraint(equalTo: redView.trailingAnchor),
-            blueView.heightAnchor.constraint(equalToConstant: 250),
+            blueView.heightAnchor.constraint(equalToConstant: 150),
             
             //yellowView
             yellowView.topAnchor.constraint(equalTo: blueView.bottomAnchor, constant: 20),
             yellowView.leadingAnchor.constraint(equalTo: blueView.leadingAnchor),
             yellowView.trailingAnchor.constraint(equalTo: blueView.trailingAnchor),
-            yellowView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            yellowView.heightAnchor.constraint(equalToConstant: 150),
+            
+            //greenView
+            greenView.topAnchor.constraint(equalTo: yellowView.bottomAnchor, constant: 20),
+            greenView.leadingAnchor.constraint(equalTo: yellowView.leadingAnchor),
+            greenView.trailingAnchor.constraint(equalTo: yellowView.trailingAnchor),
+            greenView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
 }
