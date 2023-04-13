147436358c510e9e1b57185ffdf5b515093b52c8
AlexShkunov <aws8q7@gmail.com>
2023-04-13 17:04:46 +0300

    Add gitignore

--- testAhomework/ViewController.swift
+++ testAhomework/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let redView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        layout()
     }
-
-
+    
+    private func layout() {
+        view.addSubview(redView)
+        
+        NSLayoutConstraint.activate([
+            // redView
+            redView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            redView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            redView.heightAnchor.constraint(equalToConstant: 250)
+        ])
+    }
 }
+            
 
